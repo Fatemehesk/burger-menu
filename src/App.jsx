72546dbec88d756bf26c6ff4ec5ed6a3557a20c6
Fatemehesk <fatemeh.es6717@gmail.com>
2023-02-19 09:53:47 +0200

    Adding menu burger to the app Fixes #1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
 
   return (
     <div className="App">
-      
+      <label className="hamberger-menu"><input type="checkbox"/></label>
+      <aside className="sidebar"><nav><div>Home</div><div>Search</div><div>About</div><div>Terms</div></nav></aside>
     </div>
   )
 }
